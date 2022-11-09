463e2c966f87813923e8fe3b020cce8fefe5ecbc
Chevancey <chevancey@gmail.com>
2022-11-09 04:56:22 +0100

    Added some improvements to the physics system

--- include/XEngineLibrary/PhysicsSystem.h
+++ include/XEngineLibrary/PhysicsSystem.h
@@ -1,70 +1,88 @@
-//#include "chipmunk/chipmunk.h"
-//#include "entt/entt.hpp"
-//#include "Vector2.h"
-//
-//// The Body Part
-//class RigidBodyComponent
-//{
-//
-//public:
-//
-//	RigidBodyComponent(entt::entity entity, float mass);
-//
-//private:
-//	cpBody* body;
-//};
-//
-//
-//// The Space Part
-//class PhsyicsSystem
-//{
-//
-//public:
-//	PhsyicsSystem();
-//
-//	void SetGravity(Vector2f gravity);
-//	void SetDamping(float damping);
-//
-//private:
-//	cpSpace* space;
-//};
-//
-////The Shape Part
-//class Shape
-//{
-//public:
-//
-//	Shape();
-//
-//	~Shape();
-//
-//protected:
-//	cpShape* shape;
-//};
-//
-////class CircleShape : private Shape
-////{
-////public:
-////	CircleShape(cpBody* body, float radious);
-////
-////private:
-////
-////};
-////
-////class BoxShape : private Shape
-////{
-////public:
-////	BoxShape(cpBody* body, float width, float height);
-////private:
-////
-////};
-////
-////class SegmentShape : private Shape
-////{
-////public:
-////	SegmentShape(cpBody* body, Vector2f coordOne, Vector2f coordTwo);
-////
-////private:
-////
-////};
-//
+#include "chipmunk/chipmunk.h"
+#include "entt/entt.hpp"
+#include "Vector2.h"
+
+// The Body Part
+
+
+
+ //The Space Part
+class PhsyicsSystem
+{
+
+public:
+
+	PhsyicsSystem(entt::registry& registery);
+
+	void SetGravity(Vector2f gravity);
+	void SetDamping(float damping);
+
+
+private:
+
+	cpSpace* space;
+
+};
+
+//The Shape Part
+class Shape
+{
+public:
+
+	Shape();
+
+	~Shape();
+
+protected:
+	cpShape* shape;
+};
+
+class CircleShape : private Shape
+{
+public:
+	CircleShape(cpBody* body, float radious);
+
+private:
+
+};
+
+class BoxShape : private Shape
+{
+public:
+	BoxShape(cpBody* body, float width, float height);
+private:
+
+};
+
+class SegmentShape : private Shape
+{
+public:
+	SegmentShape(cpBody* body, Vector2f coordOne, Vector2f coordTwo);
+
+private:
+
+};
+
+class RigidBodyComponent
+{
+
+public:
+	RigidBodyComponent(float mass);
+	RigidBodyComponent(const RigidBodyComponent&) = default;
+	RigidBodyComponent(RigidBodyComponent&&) = default;
+	RigidBodyComponent& operator=(const RigidBodyComponent&) = default;
+	RigidBodyComponent& operator=(RigidBodyComponent&&) = default;
+	virtual ~RigidBodyComponent();
+
+
+	RigidBodyComponent(entt::entity entity, float mass);
+
+	void AddShape();
+	void RemoveShape();
+
+private:
+
+	cpBody* body;
+	std::vector<Shape*> _shapes;
+};
+
--- src/XEngineLibrary/PhysicsSystem.cpp
+++ src/XEngineLibrary/PhysicsSystem.cpp
@@ -1,46 +1,46 @@
-//#include "PhysicsSystem.h"
-//
-//
-//PhsyicsSystem::PhsyicsSystem()
-//{
-//	space = cpSpaceNew();
-//	
-//	cpSpaceSetDamping(space, 0.5f);
-//}
-//
-//void PhsyicsSystem::SetGravity(Vector2f gravity)
-//{
-//	cpSpaceSetGravity(space, { gravity.x, gravity.y });
-//}
-//
-//void PhsyicsSystem::SetDamping(float damping)
-//{
-//	cpSpaceSetDamping(space, damping);
-//}
-//
-//
-//RigidBodyComponent::RigidBodyComponent(entt::entity entity, float mass) 
-//{
-//	body = cpBodyNew(mass, cpMomentForBox(mass, 256, 256));
-//}
-//
-//Shape::~Shape() 
-//{
-//
-//}
-//
-////CircleShape::CircleShape(cpBody* body, float radious)
-////{
-////	shape = cpCircleShapeNew(body, radious, {0.f,0.f});
-////}
-////
-////BoxShape::BoxShape(cpBody* body, float width, float height)
-////{
-////	shape = cpBoxShapeNew(body, width, height, 0.f);
-////}
-////
-////SegmentShape::SegmentShape(cpBody* body, Vector2f coordOne, Vector2f coordTwo)
-////{
-////	shape = cpSegmentShapeNew(body, { coordOne.x, coordOne.y }, { coordTwo.x, coordTwo.y }, 0.f);
-////}
-//
+#include "PhysicsSystem.h"
+
+
+PhsyicsSystem::PhsyicsSystem(entt::registry& registery)
+{
+	space = cpSpaceNew();
+	
+	cpSpaceSetDamping(space, 0.5f);
+}
+
+void PhsyicsSystem::SetGravity(Vector2f gravity)
+{
+	cpSpaceSetGravity(space, { gravity.x, gravity.y });
+}
+
+void PhsyicsSystem::SetDamping(float damping)
+{
+	cpSpaceSetDamping(space, damping);
+}
+
+
+RigidBodyComponent::RigidBodyComponent(entt::entity entity, float mass) 
+{
+	body = cpBodyNew(mass, cpMomentForBox(mass, 256, 256));
+}
+
+Shape::~Shape() 
+{
+
+}
+
+CircleShape::CircleShape(cpBody* body, float radious)
+{
+	shape = cpCircleShapeNew(body, radious, {0.f,0.f});
+}
+
+BoxShape::BoxShape(cpBody* body, float width, float height)
+{
+	shape = cpBoxShapeNew(body, width, height, 0.f);
+}
+
+SegmentShape::SegmentShape(cpBody* body, Vector2f coordOne, Vector2f coordTwo)
+{
+	shape = cpSegmentShapeNew(body, { coordOne.x, coordOne.y }, { coordTwo.x, coordTwo.y }, 0.f);
+}
+
